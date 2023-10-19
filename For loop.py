af9ad078cf430d1510dfadc48345468296d63af1
Kamron <145847421+kamron2009@users.noreply.github.com>
2023-10-19 21:11:32 +0500

    For Looop

---
+++ For loop.py
@@ -0,0 +1,68 @@
+company = ["Lg", "samsung","shivaki"]
+for x in company:
+    print(x)
+
+    
+laptops = ["hp", "acer", "lenovo", "asus", "lg"]
+for o in laptops:
+     print(o)
+
+smartphones = ["VIVO", "SAMSUNG", "APPLE", "REDMI"]
+for p in smartphones:
+   print(p)
+
+
+
+colors = ["yellow","black","white"]
+for y in colors:
+    print(y)
+    if y in "black":
+        break
+
+games = ["Pumb Mobile","Counter Strike 1.6","Stickman party"]
+for d in games:
+    print(d)
+    if d in "Counter Strike 1.6":
+        break  
+
+numbers = ["1","2","3","4"]
+for w in numbers:
+    print(w)
+    if w in "3":
+        break 
+Banks = ["Anorbank","Uzumbank","Agrobank"]
+for q in Banks:
+    print(q)
+    if q in "Anorbank":
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
