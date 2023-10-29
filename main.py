f7420c1a21e6782c2ea75c76ded7b32f2431da7d
Kamron <145847421+kamron2009@users.noreply.github.com>
2023-10-29 15:20:45 +0500

    pandas

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+mydataset = {
+     'Mashinalar': ["BMW", "Toyota Supra", "Mersades-Benz"],
+     'Narxi': [30000, 450000, 60000]
+}
+
+myvar = pd.DataFrame(mydataset)
+print(mydataset)
+
